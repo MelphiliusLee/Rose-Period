efd9ae7ab15ba43517664d039eed11870757fe6a
Yuxuan Du <aimeedu@usc.edu>
2025-01-25 15:13:33 -0800

    complete chatbot backend

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from typing import Union
+from fastapi import FastAPI, Request
+from Chatbot.rag_model import RAGModel
+from Chatbot.data import data_saver
 
-from fastapi import FastAPI
+data_saver.save_data_to_vector("data")
+rag_model = RAGModel("Chatbot/faiss_index")
 
 app = FastAPI()
 
+@app.get("/chatbot")
+def get_answer_from_chatbot(request: Request):
+    query = request.query_params.get("query")
+    if not query:
+        return {"success": 0, "message": "Please provide a question."}
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    try:
+        # 调用模型获取答案
+        answer = rag_model.get_answer(query)
+        return {"success": 1, "message": answer}
+    except Exception as e:
+        # 捕获并返回可能发生的任何异常
+        return {"success": 0, "message": f"An error occurred while processing the request: {str(e)}"}
